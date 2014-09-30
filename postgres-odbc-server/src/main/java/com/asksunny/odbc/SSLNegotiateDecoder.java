e0a38a232f6fb9a77830eb7a9c0f611c30d8631a
Developer Sunny <sunnyliu2@gmail.com>
2014-09-30 19:48:55 -0400

    add new file

--- postgres-odbc-server/src/main/java/com/asksunny/odbc/SSLNegotiateDecoder.java
+++ postgres-odbc-server/src/main/java/com/asksunny/odbc/SSLNegotiateDecoder.java
@@ -12,21 +12,19 @@ public class SSLNegotiateDecoder extends ByteToMessageDecoder {
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in,
 			List<Object> out) throws Exception 
 	{
-		 System.out.println("Hello2:" + in.readableBytes());
 		if (in.readableBytes() < 4) {
 			return;
 		}
 		in.markReaderIndex();		
 		int length = in.readInt() - 4;
-		System.out.println("Hello3:" + length + ": ready:" + in.readableBytes());
-		
+			
 		if (in.readableBytes() < length ) {
 			in.resetReaderIndex();
 			return;
 		}
 		ByteBuf msg = in.readBytes(length);
 		out.add(msg);
-		System.out.println("Hello3");
+		
 	}
 
 	
--- postgres-odbc-server/src/main/java/com/asksunny/odbc/SSLNegotiateHandler.java
+++ postgres-odbc-server/src/main/java/com/asksunny/odbc/SSLNegotiateHandler.java
@@ -1,13 +1,16 @@
 package com.asksunny.odbc;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class SSLNegotiateHandler extends ChannelHandlerAdapter  {
+public class SSLNegotiateHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
-	
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg)
+			throws Exception {
+		 ctx.writeAndFlush(new byte[]{(byte)'S'});		
+	}
 
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
