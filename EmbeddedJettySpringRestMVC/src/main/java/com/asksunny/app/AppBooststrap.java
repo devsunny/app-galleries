6a60f1387c37e570c2c1e00a462cd73a9dc32259
devsunny <sunnyliu2@gmail.com>
2015-11-14 15:14:05 -0500

    Add Hazelcast and Apache ignite example

--- EmbeddedJettySpringRestMVC/src/main/java/com/asksunny/app/AppBooststrap.java
+++ EmbeddedJettySpringRestMVC/src/main/java/com/asksunny/app/AppBooststrap.java
@@ -3,17 +3,23 @@ package com.asksunny.app;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppBooststrap {
 
-	public AppBooststrap() {		
+	public AppBooststrap() {
 	}
 
-	public static void main(String[] args) {		
-		//ApplicationContext appContext = new ClassPathXmlApplicationContext(new String[]{"server-app-context.xml"});
-		ApplicationContext appContext = new ClassPathXmlApplicationContext(new String[]{"server-app-context.xml", "ignite-context.xml"});		
-		Ignition.start(appContext.getBean(IgniteConfiguration.class));	
+	public static void main(String[] args) {
+		// ApplicationContext appContext = new
+		// ClassPathXmlApplicationContext(new
+		// String[]{"server-app-context.xml"});
+		AbstractApplicationContext appContext = new ClassPathXmlApplicationContext(
+				new String[] { "server-app-context.xml" });
+		//appContext.registerShutdownHook();
+		//appContext.close();
+		//Ignition.start(appContext.getBean(IgniteConfiguration.class));
 	}
 
 }
--- EmbeddedJettySpringRestMVC/src/main/java/com/asksunny/rest/controller/SimpleRestController.java
+++ EmbeddedJettySpringRestMVC/src/main/java/com/asksunny/rest/controller/SimpleRestController.java
@@ -1,6 +1,12 @@
 package com.asksunny.rest.controller;
 
+import java.util.Arrays;
+
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.Ignition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +14,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.asksunny.app.service.SimpleInMemoryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 
 @RestController
 public class SimpleRestController {
@@ -16,6 +27,12 @@ public class SimpleRestController {
 	@Autowired
 	private SimpleInMemoryService simpleService;
 	
+	@Autowired
+	private ApplicationContext appContext;
+	
+	@Autowired
+	@Qualifier("hazelcast")
+	private HazelcastInstance hazelcast;
 	
 	public SimpleRestController() {		
 	}
@@ -24,16 +41,43 @@ public class SimpleRestController {
 	@RequestMapping(value="/greet/{name}", method={RequestMethod.GET})
 	@ResponseBody	
 	public String greeting(@PathVariable String name)
-	{
-		
-		return String.format("Hello %s!", name);
+	{		
+		IgniteCache<String, Integer> greetCache = Ignition.ignite().cache("greetCache");			
+		Integer t = greetCache.get(name);
+		if(t==null){
+			t = new Integer(1);
+			greetCache.put(name, t);
+		}else{
+			t = new Integer(t.intValue()+1);
+			greetCache.put(name, t);
+		}
+		return String.format("Hello %s vists %d!", name, t.intValue());
 	}
+	
+	
+	@RequestMapping(value="/hazelcast/greet/{name}", method={RequestMethod.GET})
+	@ResponseBody	
+	public String hazelcastGreet(@PathVariable String name) throws Exception
+	{		
+		
+		IMap<String, Integer> greetCache = hazelcast.getMap( "greetCache" );	
+		Integer t = greetCache.get(name);
+		if(t==null){
+			t = new Integer(1);
+			greetCache.put(name, t);
+		}else{
+			t = new Integer(t.intValue()+1);
+			greetCache.put(name, t);
+		}	
+		return String.format("Hello %s %s vists %d!", name, t.intValue());
+	}
+	
 
 
 	public SimpleInMemoryService getSimpleService() {
 		return simpleService;
 	}
-
+	
 
 	public void setSimpleService(SimpleInMemoryService simpleService) {
 		this.simpleService = simpleService;
