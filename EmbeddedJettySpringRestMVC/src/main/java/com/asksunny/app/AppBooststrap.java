a92aeffad56d2375b111efc7c4f566d8453fda40
devsunny <sunnyliu2@gmail.com>
2015-11-22 15:11:01 -0500

    Add comment

--- EmbeddedJettySpringRestMVC/src/main/java/com/asksunny/app/AppBooststrap.java
+++ EmbeddedJettySpringRestMVC/src/main/java/com/asksunny/app/AppBooststrap.java
@@ -10,6 +10,14 @@ public class AppBooststrap {
 	}
 
 	public static void main(String[] args) {
+
+		/**
+		 * Doing try catch here is very important, especially spring framwork is
+		 * used to launch service service We would like to catch all startup
+		 * exception and stop the service if bean exception is every happened;
+		 * this way it would prevent unexpected behavior during the running; it
+		 * also force developer to handle exception nicely.
+		 */
 		AbstractApplicationContext appContext = null;
 		try {
 			appContext = new ClassPathXmlApplicationContext(new String[] { "server-app-context.xml" });
