38b84792ab2a7512d9253ee15fd53938992c6db3
devsunny <sunnyliu2@gmail.com>
2015-11-22 21:38:15 -0500

    Add Boostrap example

--- EmbeddedJettySpringRestMVC/src/main/java/com/asksunny/web/controller/SimpleWebController.java
+++ EmbeddedJettySpringRestMVC/src/main/java/com/asksunny/web/controller/SimpleWebController.java
@@ -2,22 +2,56 @@ package com.asksunny.web.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.asksunny.app.domain.Resource;
+import com.asksunny.app.mappers.ResourceMapper;
+import com.asksunny.app.service.ResourceManagementService;
+
 @Controller
-@RequestMapping(value="/web")
+@RequestMapping(value = "/web")
 public class SimpleWebController {
 
-	public SimpleWebController() {
-		
-	}
+	@Autowired
+	private ResourceManagementService resourceManagementService;
 	
-	@RequestMapping({"/home"})
-	public String apphome() {	    
-	    try {
+	@Autowired
+	private ResourceMapper resourceMapper;
+	
+
+	public SimpleWebController() {
+
+	}
+
+	@ModelAttribute("technologies")
+	public List<String> supportedTechnologies() {
+		List<String> list = Arrays
+				.asList(new String[] { "Spring Frameworks", "Themeleaf", "Bootstrap", "Angular", "Hazelcast", "Jetty 9.3"});
+		return list;
+	}
+
+	@ModelAttribute("resources")
+	public List<Resource> getResources() {
+
+		return resourceMapper.getResources();
+	}
+
+	@ModelAttribute("author")
+	public String getAuthor() {
+
+		return "Sunny Liu";
+	}
+
+	@RequestMapping({ "/home" })
+	public String apphome() {
+		try {
 			InputStream in = getClass().getResourceAsStream("/META-INF/views/apphome.html");
 			String text = IOUtils.toString(in);
 			System.out.println(text);
@@ -26,7 +60,23 @@ public class SimpleWebController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    return "apphome";
+		return "apphome";
+	}
+
+	public ResourceManagementService getResourceManagementService() {
+		return resourceManagementService;
+	}
+
+	public void setResourceManagementService(ResourceManagementService resourceManagementService) {
+		this.resourceManagementService = resourceManagementService;
+	}
+
+	public ResourceMapper getResourceMapper() {
+		return resourceMapper;
+	}
+
+	public void setResourceMapper(ResourceMapper resourceMapper) {
+		this.resourceMapper = resourceMapper;
 	}
 
 }
