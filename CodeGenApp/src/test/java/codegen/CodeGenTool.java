efeb1faf298e88fb954ec73d761c41acc31a878b
SunnyLiu <sunnyliu2@gmail.com>
2016-01-17 21:10:59 -0500

    Add demo application

---
+++ CodeGenApp/src/test/java/codegen/CodeGenTool.java
@@ -0,0 +1,49 @@
+package codegen;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.asksunny.codegen.CodeGenConfig;
+import com.asksunny.codegen.CodeGenConfig.CodeOverwriteStrategy;
+import com.asksunny.codegen.data.BottomUpSchemaDataGenerator;
+import com.asksunny.codegen.data.SchemaDataConfig;
+import com.asksunny.codegen.data.SchemaOutputType;
+import com.asksunny.codegen.java.JavaCodeGen;
+import com.asksunny.schema.Schema;
+import com.asksunny.schema.parser.SQLScriptLexer;
+import com.asksunny.schema.parser.SQLScriptParser;
+
+public class CodeGenTool {
+
+	@Test
+	public void genApp() throws Exception {
+		CodeGenConfig config = new CodeGenConfig();
+		config.setBaseSrcDir(".");
+		config.setBasePackageName("com.xperia.management");
+		config.setWebappContext("management");		
+		config.setAppBootstrapClassName("ManConsoleBoostrap");
+		config.setOverwriteStrategy(CodeOverwriteStrategy.OVERWRITE);
+		config.setSchemaFiles("TestAngularGen.ddl.sql");
+		JavaCodeGen javaGen = new JavaCodeGen(config);
+		javaGen.doCodeGen();
+	}
+	
+	@Test
+	@Ignore
+	public void genData() throws Exception {
+		SQLScriptLexer lexer = new SQLScriptLexer(getClass().getResourceAsStream("/TestAngularGen.ddl.sql"));
+		SQLScriptParser tokenReader = new SQLScriptParser(lexer);
+		Schema schema = tokenReader.parseSql();		
+		BottomUpSchemaDataGenerator sgen = new BottomUpSchemaDataGenerator(schema);
+		SchemaDataConfig config = new SchemaDataConfig();
+		config.setNumberOfRecords(500);
+		config.setOutputType(SchemaOutputType.INSERT);
+		config.setOutputUri("src/test/resources");
+		//config.setDebug(true);
+		sgen.setConfig(config);
+		sgen.generateData();
+	}
+
+}
---
+++ CodeGenApp/src/test/resources/TestAngularGen.ddl.sql
@@ -0,0 +1,25 @@
+create table manufacturers --#itemsPerPage=10
+(
+id bigint not null auto_increment, --#SEQUENCE,min=1
+active bit not null,  --#ENUM, values=0,1
+created_on datetime,  --#TIMESTAMP,format="yyyy-MM-dd HH:mm:ss",label="Created On"
+link_rewrite varchar(255), --#label="Rewrite",max=32
+meta_description varchar(255), --#label="Description",max=32
+meta_keywords varchar(255), --#label="Keywords",max=32
+meta_title varchar(255), --#label="Title",max=32
+name varchar(255), --#max=32
+updated_on datetime, --#label="Last Updated",TIMESTAMP,format="yyyy-MM-dd HH:mm:ss"
+primary key (id), 
+unique (name));
+
+create table Account --#itemsPerPage=10
+(
+id bigint not null primary key auto_increment , --#SEQUENCE,min=1,groupfunction=count
+Last_name varchar(32) NOT null, --#Last_NAME, label="Last Name"
+first_name varchar(32) NOT null, --#FIRST_NAME, label="FIRST Name"
+house_number int not null, --#min=1,max=100,label="House number"
+Street varchar(128) not null, --#STREET
+city varchar(128) not null, --#CITY
+state varchar(62) not null, --#STATE,drilldown=1
+zip_code varchar(10) not null --#ZIP,drilldown=2
+);
